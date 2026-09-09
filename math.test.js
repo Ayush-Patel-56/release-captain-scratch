be2130327bbea77d54bbdfc8d1377edf5ef9f67a
Ayush Patel <ayushpatel2731@gmail.com>
2026-09-09 18:02:10 +0530

    Add multiply function and test

--- math.test.js
+++ math.test.js
@@ -1,6 +1,6 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import { add, subtract } from "./math.js";
+import { add, subtract, multiply } from "./math.js";
 
 test("add", () => {
   assert.equal(add(2, 3), 5);
@@ -8,4 +8,8 @@ test("add", () => {
 
 test("subtract", () => {
   assert.equal(subtract(5, 2), 3);
+});
+
+test("multiply", () => {
+  assert.equal(multiply(4, 5), 20);
 });
